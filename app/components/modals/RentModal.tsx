81fcee8ef58ac75fcf385a8edc0613d35791596e
PIYUSH KUMAR SINGH <piyush02040@gmail.com>
2025-12-16 18:57:06 +0530

    build categories section

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -4,6 +4,9 @@ import React, { useMemo, useState } from 'react'
 import Modals from './Modals'
 import useRentModal from '@/app/hooks/useRentModel'
 import Heading from '../Heading'
+import { categories } from '../navbar/Categories'
+import CategoryInput from '../inputs/CategoryInput'
+import { FieldValues, useForm } from 'react-hook-form'
 
 enum STEPS {
     CATEGORY = 0,
@@ -20,6 +23,36 @@ const RentModal = () => {
 
     const [step,setStep] = useState(STEPS.CATEGORY)
 
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        watch,
+        formState:{
+            errors
+        },reset
+    }=useForm<FieldValues>({
+        defaultValues:{
+            category:'',
+            location:null,
+            guestCount:1,
+            roomCount:1,
+            imageSrc:'',
+            price:1,
+            title:'',
+            description:''
+        }
+    })
+
+    const category=watch('category')
+
+    const setCustomValue=(id:string,value:any)=>{
+        setValue(id,value,{
+            shouldValidate:true,
+            shouldDirty:true,
+            shouldTouch:true
+        })
+    }
     const onBack=()=>{
         setStep((prev)=>prev-1)
     }
@@ -52,10 +85,21 @@ let bodyContent=(
         />
 
         <div
-        className='grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh]'
+        className='grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto'
         
         >
-
+            {categories.map((item)=>(
+                <div key={item.label} className='col-span-1'>
+             <CategoryInput 
+              onClick={(category)=>setCustomValue('category',category)}
+             selected={category === item.label}
+             label={item.label}
+             icon={item.icon}
+             
+             
+             />
+                </div>
+            ))}
         </div>
 
     </div>
@@ -69,7 +113,7 @@ let bodyContent=(
  secondaryActionLabel={secondaryActionLabel}
  secondaryAction={step === STEPS.CATEGORY?undefined : onBack}
 onSubmit={rentModal.onClose}
-
+body={bodyContent}
 />
   )
 }
