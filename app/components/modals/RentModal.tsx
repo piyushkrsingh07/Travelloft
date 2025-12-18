04bf09c6bd73f866b4ca60a6c1807bfe5090cf26
PIYUSH KUMAR SINGH <piyush02040@gmail.com>
2025-12-18 11:33:43 +0530

    adding location filter

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,6 +7,7 @@ import Heading from '../Heading'
 import { categories } from '../navbar/Categories'
 import CategoryInput from '../inputs/CategoryInput'
 import { FieldValues, useForm } from 'react-hook-form'
+import CountrySelect from '../inputs/CountrySelect'
 
 enum STEPS {
     CATEGORY = 0,
@@ -45,6 +46,7 @@ const RentModal = () => {
     })
 
     const category=watch('category')
+    const location=watch('location')
 
     const setCustomValue=(id:string,value:any)=>{
         setValue(id,value,{
@@ -104,6 +106,23 @@ let bodyContent=(
 
     </div>
 )
+
+if(step === STEPS.LOCATION){
+    bodyContent=(
+        <div className='flex flex-col gap-8'>
+          <Heading 
+        title='Where is your place located?'
+        subtitle='Help guests find you!'
+        />
+
+        <CountrySelect
+        value={location}
+        onChange={(value)=>setCustomValue('location',value)}
+        
+        />
+        </div>
+    )
+}
   return (
 <Modals 
   isOpen={rentModal.isOpen}
@@ -112,7 +131,7 @@ let bodyContent=(
  actionLabel={actionLabel}
  secondaryActionLabel={secondaryActionLabel}
  secondaryAction={step === STEPS.CATEGORY?undefined : onBack}
-onSubmit={rentModal.onClose}
+onSubmit={onNext}
 body={bodyContent}
 />
   )
